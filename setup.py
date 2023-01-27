0d4503ef099f5438667e2cd53d9fd9c5beabc64c
Dong Wang <dong@toggle.ai>
2023-01-27 13:47:42 +0000

    ds_abc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='ds-abc',
+    name='ds_abc',
     version='0.1.0',
     description='A short description of the project.',
     author='Your name (or your organization/company/team)',
