952a5951731e55fc9035da34808e5f7a1d69c9ee
Dong Wang <dong@toggle.ai>
2023-01-27 12:41:17 +0000

    add where

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='ds-abc',
-    packages=find_packages(),
+    name='ds_abc',
+    packages=find_packages(where="src"),
     version='0.1.0',
     description='A short description of the project.',
     author='Your name (or your organization/company/team)',
