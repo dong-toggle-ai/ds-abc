e95c963c01047bf17ef7a15e44a97fe6434b730f
Dong Wang <dong@toggle.ai>
2023-01-27 11:59:00 +0000

    create subfolder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ds-abc',
-    packages=find_packages(),
+    packages=find_packages("src"),
     version='0.1.0',
     description='A short description of the project.',
     author='Your name (or your organization/company/team)',
