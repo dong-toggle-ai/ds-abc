6c6d0f29120e5886e676cd04dfd34b4b4490ad74
Dong Wang <dong@toggle.ai>
2023-01-27 13:44:35 +0000

    remove packages

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import find_packages, setup
 
 setup(
     name='ds-abc',
-    packages=find_packages(where="src"),
     version='0.1.0',
     description='A short description of the project.',
     author='Your name (or your organization/company/team)',
